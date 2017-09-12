4a129e0d5a6ca4185e525413dc503e425d311c97
Johnny Dellaluna <johnnydellaluna@gmail.com>
2017-09-11 21:34:14 -0400

    Need to edit data table next

---
+++ Train Scheduler/assets/train-app.js
@@ -0,0 +1,70 @@
+ $(document).ready(function() {
+  console.log("am i working")
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDl_FlkwOUb0Mv-vOS0Jvw0wfp00g_FjcM",
+    authDomain: "train-scheduler-5279d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5279d.firebaseio.com",
+    projectId: "train-scheduler-5279d",
+    storageBucket: "",
+    messagingSenderId: "635712118474"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    console.log("test")
+
+          // Code for handling the push
+      database.ref().push({
+        name: $("#name").val().trim(),
+        destination: $("#destination").val().trim(),
+        firsttime: $("#first-time").val().trim(),
+        frequency: $("#frequency").val().trim(),
+      });
+
+
+
+
+
+
+  })
+
+        database.ref().on("child_added", function(snapshot) {
+          // Come up with value for next arrival and minutes away
+          var nextArrival =
+
+          var minutesAway =
+
+          // Write to the table body rows of information the user puts in.
+          var newRow = "<tr> <td>" + snapshot.val().name + "</td>";
+          newRow += "<td>" + snapshot.val().destination + "</td>";
+          newRow += "<td>" + snapshot.val().frequency + "</td>";
+          newRow += "<td>" + nextArrival + "</td>";
+          newRow += "<td>" + minutesAway + "</td>";
+
+        $("#tbody").html($("#tbody").html() + newRow);
+
+
+
+
+
+
+      })
+
+        // Include moment.js time conversions to determine the two time variables
+
+        function time (date) {
+          
+
+
+
+
+
+
+
+        }
+})
